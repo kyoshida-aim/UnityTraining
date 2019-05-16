4c54b4f509c30cbc8d0280e1ed560dbbb872fdb3
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-05-16 15:18:54 +0900

    2Dで剛体運動の様子を確認するプロジェクトの作成

---
+++ Sample2/Assets/BallController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("called");
+		GetComponent<Rigidbody2D>().AddForce(new Vector2(500.0f, 300.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
