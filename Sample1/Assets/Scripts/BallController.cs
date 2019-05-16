2d58b8a84eda0d146333dc75b77d98d3ba894cff
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-05-16 16:07:36 +0900

    3Dでの物理エンジンの挙動確認
    3Dでボールを投げて床で跳ねる様子を確認したかったのですが、
    質感が悪く分かりにくかったのでやめました。
    作業途中ですが一応動作確認はできるので上げます。

---
+++ Sample1/Assets/Scripts/BallController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.ThrowBall();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ThrowBall() {
+		GetComponent<Rigidbody>().AddForce(new Vector3(0, 200, 500));
+	}
+}
