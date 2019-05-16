a9da5a8ae5c7741cf75aa867f355ab3a1272d014
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-05-16 16:06:03 +0900

    ボールを射出するスクリプトの追加

--- Sample2/Assets/BallController.cs
+++ Sample2/Assets/BallController.cs
@@ -6,8 +6,7 @@ public class BallController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log("called");
-		GetComponent<Rigidbody2D>().AddForce(new Vector2(500.0f, 300.0f));
+		GetComponent<Rigidbody2D>().AddForce(new Vector2(300.0f, 300.0f));
 	}
 	
 	// Update is called once per frame
