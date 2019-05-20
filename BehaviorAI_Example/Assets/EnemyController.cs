5d9ab34e473429e48957b337f5d5621c77559e2e
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-05-20 18:56:22 +0900

    視界(Collider)内のプレイヤーを敵が認識する機能

--- BehaviorAI_Example/Assets/EnemyController.cs
+++ BehaviorAI_Example/Assets/EnemyController.cs
@@ -7,8 +7,12 @@ public class EnemyController : MonoBehaviour {
 
 
 	public NavMeshAgent agent;
-	float span = 5.0f;
+	public GameObject target;
+
+	string state = "patrol";
+	float span = 3.0f;
 	float delta = 0;
+	float lostTimeSpan = 0.5f;
 	int patrolIndex = 0;
 	Vector3[] patrolPoint = new Vector3[4];
 
@@ -21,11 +25,50 @@ public class EnemyController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		this.delta += Time.deltaTime;
-		if (this.delta > this.span) {
-			this.delta = 0;
-			agent.SetDestination(patrolPoint[patrolIndex]);
-			patrolIndex = (patrolIndex + 1) % 4;
+		Debug.Log(this.state);
+		if (this.state == "patrol") {
+			this.delta += Time.deltaTime;
+			if (this.delta > this.span) {
+				this.delta = 0;
+				agent.SetDestination(patrolPoint[patrolIndex]);
+				patrolIndex = (patrolIndex + 1) % 4;
+			}
+		} else if (this.state == "missing") {
+			this.delta += Time.deltaTime;
+			if (this.delta > this.lostTimeSpan) {
+				this.delta = 0;
+				this.state = "lost";
+			}
+		} else if (this.state == "lost") {
+			this.delta += Time.deltaTime;
+			if (this.delta > this.lostTimeSpan) {
+				this.delta = 0;
+				this.state = "patrol";
+			}
+		}
+	}
+
+	void OnTriggerStay(Collider col) {
+		//　プレイヤーキャラクターを発見
+		if(col.tag == "Player" ) {
+			if (Physics.Linecast(transform.position,
+				col.gameObject.transform.position)) {
+				this.delta = 0;
+				this.state = "found";
+				// Debug.Log("Found");
+			} else if (this.state == "found") {
+				this.delta += Time.deltaTime;
+				if (this.delta > this.lostTimeSpan) {
+					this.delta = 0;
+					this.state = "lost";
+				}
+			}
+		}
+	}
+	
+	void OnTriggerExit(Collider col) {
+		if(col.tag == "Player" && this.state == "found") {
+				this.state = "missing";
 		}
 	}
 }
