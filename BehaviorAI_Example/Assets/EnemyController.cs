b05d5b6ef6a0648e39d2d34842b4d349883ccc73
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-05-20 16:04:22 +0900

    別ファイルでの作業をUnityファイルに移動

---
+++ BehaviorAI_Example/Assets/EnemyController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+	// const patrolPoint = 
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ BehaviorAI_Example/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+	public NavMeshAgent agent;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
+				Debug.Log(hit.point);
+				agent.SetDestination(hit.point);
+			}
+
+		}
+	}
+}
