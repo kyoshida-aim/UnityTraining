7a584cae0a7c9cacdd50e81a718788dd45672722
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-05-22 12:28:23 +0900

    サウンドテストプロジェクト作成

---
+++ SoundTest/Assets/Scenes/SoundTest/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+	NavMeshAgent agent;
+
+	void Start() {
+		this.agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
+				agent.SetDestination(hit.point);
+			}
+
+		}
+	}
+}
