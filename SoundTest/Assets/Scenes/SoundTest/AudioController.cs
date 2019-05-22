012ebc1c7e7d01eadc800252c05913a54f433e25
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-05-22 17:23:06 +0900

    コミット忘れ、変更多数

---
+++ SoundTest/Assets/Scenes/SoundTest/AudioController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+	public AudioClip se1;
+	public AudioClip se2;
+	public AudioSource se_aud;
+	public AudioSource bgm_aud;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnClick_SE1Play() {
+		this.se_aud.PlayOneShot(this.se1);
+	}
+
+	public void OnClick_SE2Play() {
+		this.se_aud.PlayOneShot(this.se2);
+	}
+
+	public void OnClick_BGMPlay() {
+		this.bgm_aud.Play();
+	}
+
+	public void OnClick_BGMStop() {
+		this.bgm_aud.Pause();
+	}
+}
---
+++ SoundTest/Assets/Scenes/SoundTest/GetSpectrumDataExample.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(AudioListener))]
+public class GetSpectrumDataExample : MonoBehaviour
+{
+    void Update()
+    {
+        float[] spectrum = new float[256];
+
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+
+		Vector3 positionfix = new Vector3(80, 30, 0);
+        for (int i = 1; i < spectrum.Length - 1; i++)
+        {
+            // Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0) + positionfix,
+			// 	new Vector3(i, spectrum[i + 1] + 10, 0) + positionfix,
+			// 	Color.red);
+            Debug.DrawLine(new Vector3(i - 1, (Mathf.Log(spectrum[i - 1]) + 10) * 10, 2) + positionfix,
+				new Vector3(i, (Mathf.Log(spectrum[i]) + 10) * 10, 2) + positionfix,
+				Color.cyan);
+            // Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1) + positionfix,
+			// 	new Vector3(Mathf.Log(i), spectrum[i] - 10, 1) + positionfix,
+			// 	Color.green);
+            // Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3) + positionfix,
+			// 	new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3) + positionfix,
+			// 	Color.blue);
+        }
+    }
+}
