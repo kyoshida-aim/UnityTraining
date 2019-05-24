448d1d226dceb16c6368efc270a22ecb41ff7b69
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-05-24 19:07:47 +0900

    メッセージの表示方法修正、いくつかのテンプレ追加

--- Sample2/Assets/RuleBaseAI/BattleText.cs
+++ Sample2/Assets/RuleBaseAI/BattleText.cs
@@ -9,10 +9,12 @@ public class BattleText : MonoBehaviour {
 	[SerializeField, Multiline] private string dealDamage = "{0} の ダメージ　を　あたえた！";
 	[SerializeField, Multiline] private string takeDamage = "{0} の ダメージ　を　うけた！";
 	[SerializeField, Multiline] private string enchantHeal = "{0} は かいふく した！";
+	[SerializeField, Multiline] private string healed = "たいりょく　が　{0} かいふく　した！";
 
 	public string BattleStart { get { return battleStart; } }
 	public string OnAttack { get { return onAttack; } }
 	public string DealDamage { get { return dealDamage; } }
 	public string TakeDamage { get { return takeDamage; } }
 	public string EnchantHeal { get { return enchantHeal; } }
+	public string Healed { get { return healed; } }
 }
