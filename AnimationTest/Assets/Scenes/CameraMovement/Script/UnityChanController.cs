916dcab22014e5ad2f82e7fc1cc4cd2038b8e8ce
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-05 16:52:00 +0900

    Cinemachineの練習
    ClearShotを利用してカメラの切り替えを行いたかったが、
    カメラからターゲットまでの距離に応じたカメラの切り替え方法がわからず
    断念。。。

---
+++ AnimationTest/Assets/Scenes/CameraMovement/Script/UnityChanController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanController : MonoBehaviour {
+
+    [SerializeField] private Animator animator;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey("up")) {
+            animator.SetBool("isRunning", true);
+            transform.position += transform.forward * 0.1f;
+        } else {
+            animator.SetBool("isRunning", false);
+        }
+        if (Input.GetKey("right")) {
+            transform.Rotate(0, 5, 0);
+        }
+        if (Input.GetKey ("left")) {
+            transform.Rotate(0, -5, 0);
+        }
+    }
+}
